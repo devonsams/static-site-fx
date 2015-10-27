3a7da56b3a7de3f17e9ad3d7a832df35ab8afff6
Sidd <ssudhe13@earlham.edu>
2015-10-27 13:51:51 -0400

    NEW SITE

--- src/js/app.js
+++ src/js/app.js
@@ -9,17 +9,106 @@ $( document ).ready(function() {
 	function resizeDiv() {
 		vpw = $(window).width();
 		vph = $(window).height();
-		$(".panels").css({"min-height": vph + 50 + "px"});
-		$("#section-2").css({"margin-top": vph + "px"});
+		$("#section-1").css({"min-height": vph + 50 + "px"});
+		//$("#section-2").css({"margin-top": vph + "px"});
 	}
 
-			
-			
-			TweenMax.from("#coffee", 1, {opacity:0, x:'-=20', y:'+=30', ease: Power2.easeInOut, force3D:true});
-			TweenMax.from("#notebook", 1, {opacity:0, x:'+=20', y:'-=30', ease: Power2.easeInOut, delay:0.2, force3D:true});
-			TweenMax.from("#mob-nav", 1.8, {opacity:0, ease: Power0.easeOut, delay:0.2, force3D:true});
-			TweenMax.from("#tagline-growth", 1, {opacity:0, ease: Power0.easeInOut, delay:1.3, force3D:true});
-			
+var controller = new ScrollMagic.Controller();
+
+// Home Animation Setup
+var header_tween = TweenMax.staggerFrom('.header-item', 1, {
+	opacity:0, 
+	x:'-=50',
+	display:"none",
+	ease: Power4.easeInOut,
+	force3D: true
+}, 0.1);
+
+// Nav Animation Setup
+var nav_tween = TweenMax.from('#mainNav', 0.7, {
+	opacity:0, 
+	y:-100,	
+	ease: Power0.easeOut,
+	display:"none",
+	delay: 1
+});
+
+// About Animation Setup
+var about_tween = TweenMax.staggerFrom('.col-md-6', 0.6, {
+		opacity:0, 
+		x:'+=200',	
+		ease: Power2.easeInOut
+	}, 0.2);
+
+// Aspire Animation Setup
+var aspire_tween = TweenMax.staggerFrom('.aspire-item', 0.4, {
+	opacity:0, 
+	x:'-=250',	
+	ease: Power2.easeOut,
+	force3D: true
+}, 0.2);
+
+// Methods Animation Setup
+var methods_tween = TweenMax.staggerFrom('.method-item', 0.4, {
+	opacity:0, 
+	x:'+=100',	
+	ease: Power4.easeOut
+}, 0.1);
+
+// Header Scene
+var header_scene = new ScrollMagic.Scene({
+  triggerElement: '.header-content'
+})
+.setTween(header_tween);
+
+// About Scene
+var about_scene = new ScrollMagic.Scene({
+  triggerElement: '#about'
+})
+.setTween(about_tween);
+
+// Aspire Scene
+var aspire_scene = new ScrollMagic.Scene({
+  triggerElement: '#aspire'
+})
+.setTween(aspire_tween);
+
+// Methods Scene
+var methods_scene = new ScrollMagic.Scene({
+  triggerElement: '#methods'
+})
+.setTween(methods_tween);
+
+controller.addScene([
+  about_scene,
+  header_scene,
+  aspire_scene,
+  methods_scene
+]);
+
+
+	// change behaviour of controller to animate scroll instead of jump
+	controller.scrollTo(function (newpos) {
+		TweenMax.to(window, 0.5, {scrollTo: {y: newpos}});
+	});
+
+	//  bind scroll to anchor links
+	$(document).on("click", "a[href^='#']", function (e) {
+		var id = $(this).attr("href");
+		if ($(id).length > 0) {
+			e.preventDefault();
+
+			// trigger scroll
+			controller.scrollTo(id);
+
+				// if supported by the browser we can even update the URL.
+			if (window.history && window.history.pushState) {
+				history.pushState("", document.title, id);
+			}
+		}
+	});
+
+
 	
 	/*$("body").queryLoader2({
 		
